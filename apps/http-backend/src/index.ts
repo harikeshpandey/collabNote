55f437a84769e7924b9f42e1f3d20a95de8c245c
harikeshpandey <harrypandey121@gmail.com>
2025-02-10 02:36:15 +0530

    First Commit

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
+const app = express();
+
+app.post("/signup" ,(req,res)=>{
+    res.json({
+        message : "signup page"
+    })
+})
+app.post("/signin" ,(req,res)=>{
+
+
+
+    const userId = 1;
+    const token = jwt.sign({userId}, JWT_SECRET);
+    res.json({
+        token
+    })
+})
+app.post("/room" ,middleware , (req,res)=>{
+    res.json({
+        roomId : 1234
+    })
+})
+
+app.listen(3001);
