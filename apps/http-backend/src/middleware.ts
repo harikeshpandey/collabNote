95a4e7253da4b6ef4a59e1a1601f3f4fab1e1cfa
harikeshpandey <harrypandey121@gmail.com>
2025-02-10 12:38:06 +0530

    Backend Updated

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
-import { JWT_SECRET } from "./config";
+import {JWT_SECRET} from "@repo/backend-common/config";
 export function middleware(req:Request , res :Response , next : NextFunction){
     const token = req.headers["authorization"]?? "";
 
