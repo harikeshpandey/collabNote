c11ba630ab496adca5fd39f71c558598094ae9c8
harikeshpandey <harrypandey121@gmail.com>
2025-02-11 08:31:55 +0530

    Setting Up Database

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,15 +2,32 @@ import express from "express";
 import jwt from "jsonwebtoken"
 import {JWT_SECRET} from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import { CreateUserSchema , SigninSchema , CreateRoomSchema} from "@repo/common/types"
+
 const app = express();
 
+
 app.post("/signup" ,(req,res)=>{
+
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "Incorrect Inputs"
+        })
+        return;
+    }
     res.json({
         message : "signup page"
     })
 })
 app.post("/signin" ,(req,res)=>{
-
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "Incorrect Inputs"
+        })
+        return;
+    }
 
 
     const userId = 1;
@@ -20,6 +37,13 @@ app.post("/signin" ,(req,res)=>{
     })
 })
 app.post("/room" ,middleware , (req,res)=>{
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "Incorrect Inputs"
+        })
+        return;
+    }
     res.json({
         roomId : 1234
     })
